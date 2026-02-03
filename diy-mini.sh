88d5a66d560e35b3e15ad7b979533547fbf82b59
xiaomeng9597 <519561910@qq.com>
2026-02-03 23:00:39 +0800

    update diy-mini.sh

--- diy-mini.sh
+++ diy-mini.sh
@@ -103,5 +103,9 @@ chmod 755 package/base-files/files/bin/coremark.sh
 git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-app-eqosplus
 
 
+#上游已经把编译器资源包删除了，先禁用吧
+sed -i 's/ci-llvm=true/ci-llvm=false/g' feeds/packages/lang/rust/Makefile
+
+
 ./scripts/feeds update -a
 ./scripts/feeds install -a
